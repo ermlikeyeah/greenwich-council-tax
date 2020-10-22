e8dd51f8d854121316c423958f623c0975ec6d73
Simon Wilson <simoncwilson@gmail.com>
2020-10-22 17:49:31 +0100

    Work from Thursday 22 October
    Carries data through to check info provided screen
    Hooked up to Notify
    Some routing logic in there

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,55 @@
+var NotifyClient = require('notifications-node-client').NotifyClient,
+    notify = new NotifyClient(process.env.NOTIFYAPIKEY);
+
 const express = require('express')
 const router = express.Router()
 
+
 // Add your routes here - above the module.exports line
+
+// The URL here needs to match the URL of the page that the user is on
+// when they type in their email address
+router.post('/Set_Up_A_Direct_Debit/submit_info', function (req, res) {
+
+  notify.sendEmail(
+    // this long string is the template ID, copy it from the template
+    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+    // in your code.
+    'a97c5ae6-1405-47c4-b67c-50d5c0e49405',
+    // `emailAddress` here needs to match the name of the form field in
+    // your HTML page
+    req.body.emailAddress
+  );
+
+  notify.sendSms(
+    // this long string is the template ID, copy it from the template
+    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+    // in your code.
+    '1ce5a5df-384a-4341-9853-21422641b2d2',
+    // `emailAddress` here needs to match the name of the form field in
+    // your HTML page
+    req.body.phoneNumber
+  );
+
+  // This is the URL the users will be redirected to once the email
+  // has been sent
+  res.redirect('/Set_Up_A_Direct_Debit/confirmation');
+
+});
+
+
+
+router.post('/Set_Up_A_Direct_Debit/correct_council_tax_address', function (req, res) {
+
+  if (req.session.data['correct-property'] == 'yes') {
+    res.redirect('your_personal_details');
+  } else if (req.session.data['correct-property'] == 'no') {
+    res.redirect('council_tax_number');
+  } else {
+    res.redirect('address_check');
+  }
+
+});
+
 
 module.exports = router
