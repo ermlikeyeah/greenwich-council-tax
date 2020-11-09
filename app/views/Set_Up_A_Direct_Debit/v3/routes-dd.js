86ef799c03070453b4e529fbf868f672ce43b9d0
Simon Wilson <simoncwilson@gmail.com>
2020-11-09 09:46:30 +0000

    Link web account to council tax

--- app/views/Set_Up_A_Direct_Debit/v3/routes-dd.js
+++ app/views/Set_Up_A_Direct_Debit/v3/routes-dd.js
@@ -19,15 +19,15 @@ router.post('/Set_Up_A_Direct_Debit/v3/submit_info', function (req, res) {
     req.body.emailAddress
   );
 
-  notify.sendSms(
+  //notify.sendSms(
     // this long string is the template ID, copy it from the template
     // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
     // in your code.
-    '1ce5a5df-384a-4341-9853-21422641b2d2',
+    //'1ce5a5df-384a-4341-9853-21422641b2d2',
     // `emailAddress` here needs to match the name of the form field in
     // your HTML page
-    req.body.phoneNumber
-  );
+    //req.body.phoneNumber
+  //);
 
   // This is the URL the users will be redirected to once the email
   // has been sent
